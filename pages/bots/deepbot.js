69b56ea115787959010cebfd4c5fe2a2b291f10e
Calix Huang <calix.huang1@gmail.com>
2020-06-23 00:08:20 -0700

    added setup

--- pages/bots/deepbot.js
+++ pages/bots/deepbot.js
@@ -1,7 +1,8 @@
-export default () => {
+export default (req, res) => {
   return (
     <>
       <h1>Deep Bot</h1>
+      <a href="https://slack.com/oauth/v2/authorize?client_id=1053072027698.1124968922742&scope=commands&user_scope="><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
     </>
   )
 }

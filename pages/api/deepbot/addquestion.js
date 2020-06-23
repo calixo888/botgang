69231548dd94dcb64bef19d282ac7db0214bcde8
Calix Huang <calix.huang1@gmail.com>
2020-06-23 00:52:38 -0700

    added addquestion

--- pages/api/deepbot/addquestion.js
+++ pages/api/deepbot/addquestion.js
@@ -1,17 +1,22 @@
+import mongoose from 'mongoose';
 import QuestionModel from './models/question.js';
 
 export default (req, res) => {
-  const question = req.body.text;
-  const author = req.body.user_name;
+  if (req.method == "POST") {
+    const question = req.body.text;
+    const author = req.body.user_name;
 
-  // ADD QUESTION TO DATABASE
-  const newQuestion = new QuestionModel({
-    question, author
-  });
-  newQuestion.save((err, newQuestion) => { if (err) throw err; });
+    // ADD QUESTION TO DATABASE
+    const newQuestion = new QuestionModel({
+      question, author
+    });
+    newQuestion.save((err, newQuestion) => { if (err) throw err; });
 
-  res.send({
-    response_type: "in_channel",
-    text: `New question added by ${author}! ${question}`
-  });
+    res.send({
+      response_type: "in_channel",
+      text: `New question added by ${author}! ${question}`
+    });
+  } else {
+    res.send("POST endpoint only.")
+  }
 }
--- pages/api/deepbot/models/question.js
+++ pages/api/deepbot/models/question.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
 
-const mongoUrl = process.env.MONGODB_URL + "/deepbot" || "mongodb://localhost:27017/deepbot";
+let mongoUrl = process.env.MONGODB_URL || "mongodb://localhost:27017";
+mongoUrl = mongoUrl + "/deepbot";
 mongoose.connect(mongoUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, '[-] Connection error'));
-const questionSchema = new mongoose.Schema({
+
+let questionSchema = new mongoose.Schema({
   question: String,
   author: String
 });
-const QuestionModel = mongoose.model("Question", questionSchema);
 
-export default QuestionModel
+export default mongoose.model("Question", questionSchema);

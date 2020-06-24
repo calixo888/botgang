683c0366f6a907b50257c61ac541dd561bb96c00
Calix Huang <calix.huang1@gmail.com>
2020-06-23 21:53:17 -0700

    done

--- pages/api/deepbot/deepquestion.js
+++ pages/api/deepbot/deepquestion.js
@@ -7,27 +7,24 @@ handler.use(middleware);
 
 handler.post(async (req, res) => {
   const questionCollection = req.db.collection("questions");
-  questionCollection.find((err, questions) => {
+
+  const questions = await questionCollection.find();
+  const question = questions[0];
+
+  console.log("questions", questions);
+  console.log("question", question);
+
+  const text = question ? `Question by ${question.author}: ${question.question}` : "No more questions in our database!";
+  console.log("text", text);
+
+  res.send({
+    response_type: "in_channel",
+    text
+  });
+
+  questionCollection.remove({ _id: question._id }, (err) => {
     if (err) throw err;
-
-    const question = questions[0];
-    console.log("questions", questions);
-    console.log("question", question);
-
-    if (question) {
-      res.send({
-        response_type: "in_channel",
-        text: `Question by ${question.author}: ${question.question}`
-      });
-    } else {
-      res.send({
-        response_type: "in_channel",
-        text: "No more questions in our database!"
-      });
-    }
-
-    // DELETE QUESTION
-    questionCollection.remove({ _id: question._id }, err => { if (err) throw err; console.log("delete"); })
+    console.log("delete");
   });
 });
 

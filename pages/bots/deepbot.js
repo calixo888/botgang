a419bd93a3c2825b078652e4136bc40b82f1cb75
Calix Huang <calix.huang1@gmail.com>
2020-06-24 22:47:03 -0700

    hide deepbot client ID

--- pages/bots/deepbot.js
+++ pages/bots/deepbot.js
@@ -1,8 +1,11 @@
 export default (req, res) => {
+  const client_id = process.env.DEEPBOT_CLIENT_ID;
+  const oAuthLink = `https://slack.com/oauth/v2/authorize?client_id=${client_id}&scope=commands&user_scope=`
+
   return (
     <>
       <h1>Deep Bot</h1>
-      <a href="https://slack.com/oauth/v2/authorize?client_id=1053072027698.1124968922742&scope=commands&user_scope="><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
+      <a href={oAuthLink}><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
     </>
   )
 }

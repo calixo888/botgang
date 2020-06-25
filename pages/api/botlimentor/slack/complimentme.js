50727cc68f9b7e0a13c37dc553a837044de93e21
Calix Huang <calix.huang1@gmail.com>
2020-06-25 14:30:03 -0700

    added complimentme

---
+++ pages/api/botlimentor/slack/complimentme.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+import nextConnect from 'next-connect';
+import middleware from '../../../../middleware/database';
+
+const handler = nextConnect();
+
+handler.use(middleware);
+
+handler.post(async (req, res) => {
+  const response = await axios.get("https://responser.com/api");
+
+  res.send({
+    response_type: "in_channel",
+    text: response.compliment
+  });
+});
+
+export default handler;

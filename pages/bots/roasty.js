f075549fd618407ba9c857d871705f9465875d32
Calix Huang <calix.huang1@gmail.com>
2020-06-24 23:06:25 -0700

    typo

--- pages/bots/roasty.js
+++ pages/bots/roasty.js
@@ -4,7 +4,7 @@ export default (req, res) => {
 
   return (
     <>
-      <h1>Roasy</h1>
+      <h1>Roasty</h1>
       <a href={oAuthLink}><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
     </>
   )

509e90ee715389fd9d4c8b1e3fefd841dcd07384
Calix Huang <calix.huang1@gmail.com>
2020-06-26 12:48:46 -0700

    added content to about

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Layout from '../components/layouts/Layout'
 
 export default () => {
@@ -9,30 +10,32 @@ export default () => {
 
         <div>
           <h1>Calix Huang</h1>
-          <p className="mb-2"><strong>Founder, Lead Engineer</strong></p>
+          <p className="mb-2"><strong>The Gang Leader</strong></p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            My name's Calix Huang, and I'm the Gang Leader. Whatever I say, goes. No questions, no comments, no concerns. I'm the baddest of the bad, the edgiest of the edgy. No one ever talks to me without shedding a single tear, and no one ever leaves my sight without mental trauma. I am the epitome of intimidation, and that's the way it shall stay.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            If you want in to the Bot Gang, you'll have to go through me first. No free rides, no participation awards, and <strong>no wimps</strong>. We are here to take over, and we don't want softies in the gang to slow us down. If you truly think you can handle the Bot Gang, <Link href="/contact">
+              <a>send us a message</a>
+            </Link>, and I'll consider giving you a shot.
           </p>
           <div className="social-icons">
-            <a href="#" target="_blank">
+            <a href="https://www.facebook.com/calix.huang.904" target="_blank">
               <img src="/img/icons/facebook.svg" />
             </a>
-            <a href="#" target="_blank">
+            <a href="https://www.instagram.com/calix_huang/" target="_blank">
               <img src="/img/icons/instagram.svg" />
             </a>
-            <a href="#" target="_blank">
+            <a href="https://www.linkedin.com/in/calix-huang/" target="_blank">
               <img src="/img/icons/linkedin.svg" />
             </a>
-            <a href="#" target="_blank">
+            <a href="https://github.com/calixo888" target="_blank">
               <img src="/img/icons/github.svg" />
             </a>
-            <a href="#" target="_blank">
+            <a href="mailto:calix@botgang.tech" target="_blank">
               <img src="/img/icons/email.svg" />
             </a>
-            <a href="#" target="_blank">
+            <a href="https://www.calix.dev/" target="_blank">
               <img src="/img/icons/website.svg" />
             </a>
           </div>

775ceeb20ddb3caedbacf2c0f544985ef254978b
Calix Huang <calix.huang1@gmail.com>
2020-06-26 22:04:03 -0700

    refresh

--- pages/api/hyperlinky/slack/setup.js
+++ pages/api/hyperlinky/slack/setup.js
@@ -8,7 +8,7 @@ export default (req, res) => {
     if (req.query.code) {
       axios.get(`https://slack.com/api/oauth.v2.access?client_id=${client_id}&client_secret=${client_secret}&code=${req.query.code}`).then((response) => {
         res.writeHead(301, {
-          Location: '/bots/hyperlinky'
+          Location: '/bots/hyperlinky' 
         });
         res.end();
       })
